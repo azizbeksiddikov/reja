8e3e3a82b131b510f513cca3a85465624858cdce
andrew <azbeksid@gmail.com>
2024-11-21 13:11:14 +0900

    BRR: train.js added async and callback

--- train.js
+++ train.js
@@ -9,83 +9,8 @@ const list = [
   "endi dam oling, foydasi yoq endi", // 60
 ];
 
-// ************************* Callback functions
-// async function maslahatBering(a, callback) {
-//   if (typeof a !== "number") throw new Error("Iltimos, insert number", null);
-//   else if (a <= 20) return list[0];
-//   else if (a <= 30) return list[1];
-//   else if (a <= 40) return list[2];
-//   else if (a <= 50) return list[3];
-//   else if (a <= 60) return list[4];
-//   else {
-//     setInterval(() => {
-//       callback(null, list[5]);
-//     }, 1000);
-//   }
-// }
+// *********************** Event Loop and Callbacks
 
-// console.log("passed here 0");
-// maslahatBering(70, (err, data) => {
-//   if (err) console.log("error:", err);
-//   console.log("Result:", data);
-// });
-// console.log("passed here 1");
-
-// ************************* Async functions:
-// async function maslahatBering(a, callback) {
-//   if (typeof a !== "number") throw new Error("Iltimos, insert number", null);
-//   else if (a <= 20) return list[0];
-//   else if (a <= 30) return list[1];
-//   else if (a <= 40) return list[2];
-//   else if (a <= 50) return list[3];
-//   else if (a <= 60) return list[4];
-//   else {
-//     return new Promise((resolve, reject) => {
-//       setTimeout(() => {
-//         resolve(list[5]);
-//       }, 5000);
-//     });
-//   }
-// }
-
-// async function maslahatBering(a, callback) {
-//   if (typeof a !== "number") throw new Error("Iltimos, insert number", null);
-//   else if (a <= 20) return list[0];
-//   else if (a <= 30) return list[1];
-//   else if (a <= 40) return list[2];
-//   else if (a <= 50) return list[3];
-//   else if (a <= 60) return list[4];
-//   else list[5];
-// }
-
-// ************************* Call via then, catch:
-// console.log("passed here 0");
-// maslahatBering(20)
-//   .then((data) => {
-//     console.log("Result:", data);
-//   })
-//   .catch((err) => {
-//     console.log("error:", err);
-//   });
-// console.log("passed here 1");
-
-// ************************* Call via async/await:
-// async function run() {
-//   let res = await maslahatBering(65);
-//   console.log("Result:", res);
-
-//   res = await maslahatBering(30);
-//   console.log("Result:", res);
-
-//   res = await maslahatBering(83);
-//   console.log("Result:", res);
-
-//   res = await maslahatBering(55);
-//   console.log("Result:", res);
-// }
-// run();
-
-// ************************* Callback:
 // function maslahatBering(a, callback) {
 //   if (typeof a !== "number") callback("Iltimos, insert number", null);
 //   else if (a <= 20) callback(null, list[0]);
@@ -101,7 +26,99 @@ const list = [
 // }
 
 // console.log("passed here 0");
-// maslahatBering(65, (err, data) => {
+// maslahatBering(70, (err, data) => {
+//   if (err) console.log("error:", err);
+//   console.log("Result:", data);
+// });
+// console.log("passed here 1");
+
+// // *********************** Asynchronus functions
+// async function maslahatBering(a) {
+//   if (typeof a !== "number") throw new Error("Iltimos, insert number", null);
+//   else if (a <= 20) return list[0];
+//   else if (a <= 30) return list[1];
+//   else if (a <= 40) return list[2];
+//   else if (a <= 50) return list[3];
+//   else if (a <= 60) return list[4];
+//   else return list[5];
+// }
+
+// // ************************* Call via then/catch:
+// console.log("passed here 0");
+// maslahatBering(20)
+//   .then((data) => {
+//     console.log("Result:", data);
+//   })
+//   .catch((err) => {
+//     console.log("Error:", err);
+//   });
+// console.log("passed here 1");
+
+// // ************************* Call via async/await:
+// async function run() {
+//   let res = await maslahatBering(65);
+//   console.log("Result:", res);
+
+//   res = await maslahatBering(30);
+//   console.log("Result:", res);
+
+//   res = await maslahatBering(83);
+//   console.log("Result:", res);
+
+//   res = await maslahatBering(55);
+//   console.log("Result:", res);
+// }
+// run();
+
+// // *********************** Promise functions
+// async function maslahatBering(a, callback) {
+//   if (typeof a !== "number") throw new Error("Iltimos, insert number", null);
+//   else if (a <= 20) return list[0];
+//   else if (a <= 30) return list[1];
+//   else if (a <= 40) return list[2];
+//   else if (a <= 50) return list[3];
+//   else if (a <= 60) return list[4];
+//   else {
+//     return new Promise((resolve, reject) => {
+//       setTimeout(() => {
+//         resolve(list[5]);
+//       }, 3000);
+//     });
+//   }
+// }
+
+// async function run() {
+//   let res = await maslahatBering(65);
+//   console.log("Result:", res);
+
+//   res = await maslahatBering(30);
+//   console.log("Results:", res);
+
+//   res = await maslahatBering(83);
+//   console.log("Result:", res);
+
+//   res = await maslahatBering(55);
+//   console.log("Result:", res);
+// }
+// run();
+
+// // setInterval with Callback
+// function maslahatBering(a, callback) {
+//   if (typeof a !== "number") callback("Iltimos, insert number", null);
+//   else if (a <= 20) callback(null, list[0]);
+//   else if (a <= 30) callback(null, list[1]);
+//   else if (a <= 40) callback(null, list[2]);
+//   else if (a <= 50) callback(null, list[3]);
+//   else if (a <= 60) callback(null, list[4]);
+//   else {
+//     setInterval(function () {
+//       callback(null, list[5]);
+//     }, 3000);
+//   }
+// }
+
+// console.log("passed here 0");
+// maslahatBering(70, (err, data) => {
 //   if (err) console.log("error:", err);
 //   console.log("Result:", data);
 // });
