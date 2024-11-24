733cce49a58327930c18e2035a0654291d644633
andrew <azbeksid@gmail.com>
2024-11-24 14:31:41 +0900

    feat: added task_b

--- train.js
+++ train.js
@@ -1,13 +1,30 @@
+// B-TASK:
+
+// Shunday function tuzing, u 1ta string parametrga ega bolsin, hamda osha stringda qatnashgan raqamlarni sonini bizga return qilsin.
+// MASALAN countDigits("ad2a54y79wet0sfgb9") 7ni return qiladi.
+// masalani yechimi:
+
+function countDigits(word) {
+  return word.split("").reduce((count, char) => {
+    return char >= "0" && char <= "9" ? count + 1 : count;
+  }, 0);
+}
+
+let result = countDigits("ad2a54y79wet0sfgb9"); // 7
+console.log(result);
+result = countDigits("2024 is the year!"); // 4
+console.log(result);
+
 // console.log("Jack Ma maslahatlari");
 
-const list = [
-  "yaxshi talaba boling", // 0-20
-  "togri boshliq tanlang va koproq hato qiling", // 20-30
-  "uzingizga ishlashingizni boshlang", // 30-40
-  "siz kuchli bolgan narsalarni qiling", // 40-50
-  "yoshlarga investitsiya qiling", // 50-60
-  "endi dam oling, foydasi yoq endi", // 60
-];
+// const list = [
+//   "yaxshi talaba boling", // 0-20
+//   "togri boshliq tanlang va koproq hato qiling", // 20-30
+//   "uzingizga ishlashingizni boshlang", // 30-40
+//   "siz kuchli bolgan narsalarni qiling", // 40-50
+//   "yoshlarga investitsiya qiling", // 50-60
+//   "endi dam oling, foydasi yoq endi", // 60
+// ];
 
 // *********************** Event Loop and Callbacks
 
@@ -103,26 +120,26 @@ const list = [
 // run();
 
 // setInterval with Callback
-function maslahatBering(a, callback) {
-  if (typeof a !== "number") callback("Iltimos, insert number", null);
-  else if (a <= 20) callback(null, list[0]);
-  else if (a <= 30) callback(null, list[1]);
-  else if (a <= 40) callback(null, list[2]);
-  else if (a <= 50) callback(null, list[3]);
-  else if (a <= 60) callback(null, list[4]);
-  else {
-    setInterval(function () {
-      callback(null, list[5]);
-    }, 3000);
-  }
-}
+// function maslahatBering(a, callback) {
+//   if (typeof a !== "number") callback("Iltimos, insert number", null);
+//   else if (a <= 20) callback(null, list[0]);
+//   else if (a <= 30) callback(null, list[1]);
+//   else if (a <= 40) callback(null, list[2]);
+//   else if (a <= 50) callback(null, list[3]);
+//   else if (a <= 60) callback(null, list[4]);
+//   else {
+//     setInterval(function () {
+//       callback(null, list[5]);
+//     }, 3000);
+//   }
+// }
 
-console.log("passed here 0");
-maslahatBering(70, (err, data) => {
-  if (err) console.log("error:", err);
-  console.log("Result:", data);
-});
-console.log("passed here 1");
+// console.log("passed here 0");
+// maslahatBering(70, (err, data) => {
+//   if (err) console.log("error:", err);
+//   console.log("Result:", data);
+// });
+// console.log("passed here 1");
 
 // A-TASK:
 // Shunday 2 parametrli function tuzing, hamda birinchi parametrdagi letterni ikkinchi parametrdagi sozdan qatnashga sonini return qilishi kerak boladi.
